93a74df042c4fee3b9ab9a0d16bb3548f9538913
SergeyZinkovich <my_agent-1@mail.ru>
2022-07-09 00:45:58 +1000

    Add increase date in sort order option to change_taken_date_for_all

--- metadata_setter.py
+++ metadata_setter.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import piexif
 import os
 import pythoncom
@@ -27,27 +27,35 @@ def change_video_taken_date(filename: str, taken_date: datetime):
     properties.Commit()
 
 
-def change_taken_date(filename: str, taken_date: datetime, change_access_time: bool = False,
-                      set_taken_date_to_access_time: bool = True):
-    if os.path.splitext(filename)[-1] in ['.jpg', '.JPG']:
+def change_taken_date(filename: str, taken_date: datetime, change_modification_time: bool = False,
+                      set_taken_date_to_access_time: bool = False):
+    extension = os.path.splitext(filename)[-1]
+    if extension in ['.jpg', '.JPG']:
         func = change_photo_taken_date
-    if os.path.splitext(filename)[-1] in ['.mp4', '.MOV', '.3gp']:
+    elif extension in ['.mp4', '.MOV', '.3gp']:
         func = change_video_taken_date
+    else:
+        print(f'Unrecognized extension {extension}')
+        return
 
     access_and_modification_time = (os.stat(filename).st_atime, os.stat(filename).st_mtime)
 
     func(filename, taken_date)
 
-    if not change_access_time:
+    if not change_modification_time:
         os.utime(filename, access_and_modification_time)
 
     if set_taken_date_to_access_time:
         os.utime(filename, (taken_date.timestamp(), taken_date.timestamp()))
 
 
-def change_taken_date_for_all(base_path: str, tree: dict, taken_date: datetime, change_access_time: bool = False,
-                              set_taken_date_to_access_time: bool = True):
-    for path, data in tree.items():
-        for filename in data[1]:
-            change_taken_date(os.path.join(base_path, path, filename), taken_date, change_access_time,
-                              set_taken_date_to_access_time)
+def change_taken_date_for_all(paths: list, taken_date: datetime, increase_date_in_sort_order: bool = False,
+                              date_step: datetime = timedelta(minutes=1), paths_sort_key=os.path.getctime,
+                              change_modification_time: bool = False, set_taken_date_to_access_time: bool = False):
+    paths = sorted(paths, key=lambda x: (paths_sort_key, x))
+
+    for path in paths:
+        if increase_date_in_sort_order:
+            taken_date += date_step
+
+        change_taken_date(path, taken_date, change_modification_time, set_taken_date_to_access_time)
